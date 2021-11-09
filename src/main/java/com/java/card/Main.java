e2a6e8ce87057db6aee3613d260e87abaa2f50ec
xeroman.k <xeroman.k@itsmart.co.kr>
2021-11-09 15:17:48 +0900

    fix test

--- src/main/java/com/java/card/Main.java
+++ src/main/java/com/java/card/Main.java
@@ -37,6 +37,7 @@ public class Main {
 //        System.out.println(user.straight());
 //        System.out.println(user.flush());
         System.out.println(user.pair(3));
+        System.out.println(user.pair(4));
     }
 
 }
